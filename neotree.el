f316fa25ea66a93c250789ae468346fb11af4299
Mr Speaker <mrspeaker@gmail.com>
2019-06-02 08:47:08 -0400

    Remove spacing tabs. Left-chevron instead of down.
    The "down chevron" is wider than the right chevron - so it pushes
    things out. I'll just use left instead!
    
    Also, changed the tab stops to spaces - files aren't lined up, but
    less noticable than tabs.

--- neotree.el
+++ neotree.el
@@ -1264,9 +1264,9 @@ Optional NODE-NAME is used for the `icons' theme"
       (unless (require 'all-the-icons nil 'noerror)
         (error "Package `all-the-icons' isn't installed"))
       (setq-local tab-width 1)
-      (or (and (equal name 'open)  (insert (all-the-icons-icon-for-dir (directory-file-name node-name) "down")))
-          (and (equal name 'close) (insert (all-the-icons-icon-for-dir (directory-file-name node-name) "right")))
-          (and (equal name 'leaf)  (insert (format "\t\t\t%s\t" (all-the-icons-icon-for-file node-name))))))
+      (or (and (equal name 'open)  (insert (all-the-icons-icon-for-dir (directory-file-name node-name) "left" " ")))
+          (and (equal name 'close) (insert (all-the-icons-icon-for-dir (directory-file-name node-name) "right" " ")))
+          (and (equal name 'leaf)  (insert (format "\t\t\t%s " (all-the-icons-icon-for-file node-name))))))
      (t
       (or (and (equal name 'open)  (funcall n-insert-symbol "- "))
           (and (equal name 'close) (funcall n-insert-symbol "+ ")))))))
